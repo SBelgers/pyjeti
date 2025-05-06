47d55f1778e4103c8a00d73d0dd47e259850ecf6
S. Belgers <s.belgers@tue.nl>
2025-05-06 14:54:33 +0200

    ensure data files are included.

--- setup.py
+++ setup.py
@@ -5,4 +5,10 @@ setup(
     version="0.1",
     packages=["pyjeti"],
     install_requires=["tqdm", "numpy"],
+    package_data={
+        "pyjeti": [
+            "jeti_drivers/*",
+            "jeti_error_codes.json",
+        ]
+    },
 )
