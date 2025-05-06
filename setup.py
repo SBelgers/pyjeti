0498a62fd2e757d74a405df150cfdd6bacaa517f
S. Belgers <s.belgers@tue.nl>
2025-05-06 15:13:35 +0200

    Fixed the wildcard

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     install_requires=["tqdm", "numpy"],
     package_data={
         "pyjeti": [
-            "jeti_drivers/*",
+            "jeti_drivers\Win32\jeti_spectro_ex.dll",
+            "jeti_drivers\Win64\jeti_spectro_ex64.dll",
             "jeti_error_codes.json",
         ]
     },
