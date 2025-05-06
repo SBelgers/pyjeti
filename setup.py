9942082a83c13170dc4b95bc7769b65e14719457
S. Belgers <s.belgers@tue.nl>
2025-05-06 15:30:10 +0200

    import using MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,13 +5,5 @@ setup(
     version="0.1",
     packages=["pyjeti"],
     install_requires=["tqdm", "numpy"],
-    package_data={
-        "pyjeti": [
-            "jeti_drivers\Win32\jeti_spectro_ex.dll",
-            "jeti_drivers\Win32\Lib\jeti_radio_ex.lib",
-            "jeti_drivers\Win64\jeti_spectro_ex64.dll",
-            "jeti_drivers\Win64\Lib\jeti_radio_ex64.lib",
-            "jeti_error_codes.json",
-        ]
-    },
+    include_package_data=True,
 )
